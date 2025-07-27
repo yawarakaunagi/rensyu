f7f5b91d39e5771bc5d9a76d665d7db1fd34c083
yawarakaunagi <yawarakaunagi@gmail.com>
2025-07-27 10:36:38 +0900

    PILモジュールをインポートし、コードの可読性を向上させました。

--- practice.py
+++ practice.py
@@ -1,9 +1,13 @@
 import os
 import glob
+from PIL import Image
+
 
 def get_files(folder):
     for file in glob.glob(folder):
         yield file
+
+
 
 for file in get_files("C:/Users/katay/Documents/images/01/jpeg/*.jpg"):
     print(file)
