172e92ffca5c3b0fcab6675ccc3209064e08f1e4
yawarakaunagi <yawarakaunagi@gmail.com>
2025-07-27 10:30:27 +0900

    不要なコードを削除し、画像ファイルを取得する関数を追加しました。

--- practice.py
+++ practice.py
@@ -1,6 +1,9 @@
-import numbers
 import os
+import glob
 
-print("テストです。")
-for i in range(10):
-    print(i+3)
+def get_files(folder):
+    for file in glob.glob(folder):
+        yield file
+
+for file in get_files("C:/Users/katay/Documents/images/01/jpeg/*.jpg"):
+    print(file)
